70b81be11a16d846ef55554a54af4eebc4f8b573
rejka permana <permanarejka@gmail.com>
2023-11-06 18:20:29 +0700

    Update layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   root "home#index"
   controller :home do
     get :index
